2166f0e0fccd4a50165a2683bd670b0af8cb1ef1
Torres Ng <torresng2684cte@gmail.com>
2019-03-29 11:37:32 +0800

    insertion sort for template

--- rust/insertion_sort/src/lib.rs
+++ rust/insertion_sort/src/lib.rs
@@ -1,10 +1,11 @@
-pub fn insertion_sort(arr: &mut [i32]) {
+pub fn insertion_sort<T>(arr: &mut [T])
+where
+    T: Ord,
+{
     for j in 1..arr.len() {
         for i in (0..j).rev() {
             if arr[i] > arr[i + 1] {
-                let key = arr[i + 1];
-                arr[i + 1] = arr[i];
-                arr[i] = key;
+                arr.swap(i, i + 1)
             } else {
                 break;
             }
