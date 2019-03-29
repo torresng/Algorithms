2183492af46f5e308164f74f56592afab80838a5
Torres Ng <torresng2684cte@gmail.com>
2019-03-29 12:03:58 +0800

    template for selection_sort

--- cpp/selection_sort/main.cpp
+++ cpp/selection_sort/main.cpp
@@ -1,15 +1,15 @@
-#include <array>
 #include <algorithm>
+#include <array>
 #include <iostream>
 
 using namespace std;
 
-void selection_sort(array<int, 6> &arr) {
+template <typename T, int N> void selection_sort(array<T, N> &arr) {
   int n = arr.size();
-  for(int j = 0; j < n - 1; j++) {
+  for (int j = 0; j < n - 1; j++) {
     int smallest = j;
-    for(int i = j + 1; i < n; i++) {
-      if(arr[i] < arr[smallest]) {
+    for (int i = j + 1; i < n; i++) {
+      if (arr[i] < arr[smallest]) {
         smallest = i;
       }
     }
@@ -17,14 +17,18 @@ void selection_sort(array<int, 6> &arr) {
   }
 }
 
-int main(void) { 
+void test_case_1() {
   std::array<int, 6> arr{5, 2, 4, 6, 1, 3};
-  selection_sort(arr);
+  selection_sort<int, 6>(arr);
+  std::array<int, 6> result{1, 2, 3, 4, 5, 6};
 
-  for (int elem : arr) {
-    std::cout << elem << " ";
+  for (int i = 0; i < arr.size(); i++) {
+    assert(arr[i] == result[i]);
   }
-  std::cout << std::endl;
+}
 
-  return 0; 
+int main(void) {
+  test_case_1();
+
+  return 0;
 }
