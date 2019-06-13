447de8dc964e3f20b314c2ac15b5c7865e4f8d5e
Torres Ng <torresng2684cte@gmail.com>
2019-06-13 09:53:32 +0800

    merge sort

--- cpp/merge_sort/main.cpp
+++ cpp/merge_sort/main.cpp
@@ -1,8 +1,7 @@
 #include <cstdio>
+#include <iostream>
 
-using namespace std;
-
-const int N = 1e6 + 10;
+const int N = 10e7 + 10;
 
 int arr[N], tmp[N];
 
@@ -15,7 +14,7 @@ void merge_sort(int arr[], int l, int r) {
 
     merge_sort(arr, l, mid), merge_sort(arr, mid + 1, r);
 
-    int k = 0, i = l, j = mid + 1;
+    int i = l, j = mid + 1, k = 0;
     while (i <= mid && j <= r) {
         if (arr[i] <= arr[j]) {
             tmp[k++] = arr[i++];
@@ -30,8 +29,8 @@ void merge_sort(int arr[], int l, int r) {
         tmp[k++] = arr[j++];
     }
 
-    for (i = l, j = 0; i <= r; ++i, ++j) {
-        arr[i] = tmp[j];
+    for (i = l, k = 0; i <= r; ++i, ++k) {
+        arr[i] = tmp[k];
     }
 }
 
