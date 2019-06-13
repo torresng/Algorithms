74b2b3cd7b4b6220323aaade1a65abb43a47a258
Torres Ng <torresng2684cte@gmail.com>
2019-06-13 13:40:21 +0800

    binarySerach

--- cpp/binary_search/main.cpp
+++ cpp/binary_search/main.cpp
@@ -3,6 +3,41 @@
 
 using namespace std;
 
+bool check(int mid);
+/**
+ * 二分模板 - 版本1
+ * 當我們將區間[l, r]划分成[l, mid]和[mid + 1, r]時，
+ * 其更新操作是r = mid或者l = mid + 1，計算mid時不需要加1。
+ */
+int bSearch_1(int l, int r) {
+    while (l < r) {
+        int mid = (l + r) >> 1;
+        if (check(mid)) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return l;
+}
+
+/**
+ * 二分模板 - 版本2
+ * 當我們將區間[l, r]划分成[l, mid - 1]和[mid, r]時，
+ * 其更新操作是r = mid - 1或者l = mid，此時為了防止死循環，計算mid時需要加1。
+ */
+int bSearch_2(int l, int r) {
+    while (l < r) {
+        int mid = (l + r + 1) >> 1;
+        if (check(mid)) {
+            l = mid;
+        } else {
+            r = mid - 1;
+        }
+    }
+    return l;
+}
+
 /**
  * 二分查找，找到該值在數組中的下標，否則為-1
  */
