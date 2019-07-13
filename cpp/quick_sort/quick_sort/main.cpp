033cb14298130d072fa417eb596d4cda2a009bae
Torres Ng <torresng2684cte@gmail.com>
2019-07-13 21:08:55 +0800

    quick sort

--- cpp/quick_sort/quick_sort/main.cpp
+++ cpp/quick_sort/quick_sort/main.cpp
@@ -1,19 +1,17 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 const int N = 1000010;
 
-int arr[N];
-
 void quick_sort(int arr[], int l, int r) {
-    if (l >= r) {
+    if (l == r) {
         return;
     }
 
     int i = l - 1, j = r + 1, x = arr[(l + r) >> 1];
-
     while (i < j) {
         do {
             ++i;
@@ -30,10 +28,11 @@ void quick_sort(int arr[], int l, int r) {
     quick_sort(arr, j + 1, r);
 }
 
-int main(void) {
+int main() {
     int n;
     scanf("%d", &n);
 
+    int arr[N];
     for (int i = 0; i < n; ++i) {
         scanf("%d", &arr[i]);
     }
@@ -41,7 +40,7 @@ int main(void) {
     quick_sort(arr, 0, n - 1);
 
     for (int i = 0; i < n; ++i) {
-        printf("%d", arr[i]);
+        printf("%d ", arr[i]);
     }
 
     return 0;
