19db1dee00a065a993dd7bd662f80bd783243485
Torres Ng <torresng2684cte@gmail.com>
2019-07-27 17:30:17 +0800

    double linked list

---
+++ cpp/linked_list/double_linked_list/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int N = 100010;
+
+int e[N], l[N], r[N], idx;
+
+// 在節點a的右邊插入一個數x
+void insert(int a, int x) {
+    e[idx] = x;
+    l[idx] = a, r[idx] = r[a];
+    l[r[a]] = idx, r[a] = idx++;
+}
+
+// 刪除節點a
+void remove(int a) {
+    l[r[a]] = l[a];
+    r[l[a]] = r[a];
+}
+
+int main() {
+    int m;
+    cin >> m;
+
+    r[0] = 1, l[1] = 0;
+    idx = 2;
+
+    while (m--) {
+        string op;
+        cin >> op;
+        int k, x;
+        if (op == "L") {
+            cin >> x;
+            insert(0, x);
+        } else if (op == "R") {
+            cin >> x;
+            insert(l[1], x);
+        } else if (op == "D") {
+            cin >> k;
+            remove(k + 1);
+        } else if (op == "IL") {
+            cin >> k >> x;
+            insert(l[k + 1], x);
+        } else {
+            cin >> k >> x;
+            insert(k + 1, x);
+        }
+    }
+
+    for (int i = r[0]; i != 1; i = r[i])
+        cout << e[i] << ' ';
+    cout << endl;
+
+    return 0;
+}
