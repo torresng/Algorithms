681867e7a25c13ed5b6bc07e00abf6e814fc0704
Torres Ng <torresng2684cte@gmail.com>
2019-07-30 13:57:58 +0800

    Monotone Stack

---
+++ cpp/monotone_stack/monotone_stack/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 100010;
+
+int stk[N], tt;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        while (tt && stk[tt] >= x) {
+            tt--;
+        }
+        if (tt) {
+            cout << stk[tt] << ' ';
+        } else {
+            cout << "-1 ";
+        }
+        stk[++tt] = x;
+    }
+    cout << endl;
+
+    return 0;
+}
