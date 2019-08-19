d92011a9bb07641ef84e2e2144fac5c203e49e64
Torres Ng <torresng2684cte@gmail.com>
2019-08-19 21:00:12 +0800

    Trie

---
+++ cpp/Trie/number_of_points_in_the_connected_block/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int n, m;
+int p[N], size[N];
+
+int find(int x) {
+    if (p[x] != x) {
+        p[x] = find(p[x]);
+    }
+    return p[x];
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+
+    for (int i = 1; i <= n; i++) {
+        p[i] = i;
+        size[i] = 1;
+    }
+
+    while (m--) {
+        char op[5];
+        int a, b;
+        scanf("%s", op);
+
+        if (op[0] == 'C') {
+            scanf("%d%d", &a, &b);
+            if (find(a) == find(b)) {
+                continue;
+            }
+            size[find(b)] += size[find(a)];
+            p[find(a)] = find(b);
+        } else if (op[1] == '1') {
+            scanf("%d%d", &a, &b);
+            if (find(a) == find(b)) {
+                puts("Yes");
+            } else {
+                puts("No");
+            }
+        } else {
+            scanf("%d", &a);
+            printf("%d\n", size[find(a)]);
+        }
+    }
+
+    return 0;
+}
