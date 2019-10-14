b4eccfc3cb91fba6dddc68c5b89b4742d5b359ec
Torres Ng <torresng2684cte@gmail.com>
2019-10-14 20:17:30 +0800

    Nim游戏

---
+++ cpp/博弈论/Nim游戏/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int res = 0;
+
+    scanf("%d", &n);
+    while (n--) {
+        int x;
+        scanf("%d", &x);
+        res ^= x;
+    }
+
+    if (res) {
+        puts("Yes");
+    } else {
+        puts("No");
+    }
+
+    return 0;
+}
