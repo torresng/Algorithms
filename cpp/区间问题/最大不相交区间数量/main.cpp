b8c91d9fccd76016efb01c6fe4b915e17fdbc5e5
Torres Ng <torresng2684cte@gmail.com>
2019-11-15 12:12:00 +0800

    区间问题

---
+++ cpp/区间问题/最大不相交区间数量/main.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int n;
+struct Range {
+    int l, r;
+    bool operator<(const Range &W) const { return r < W.r; }
+} range[N];
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        range[i] = {l, r};
+    }
+
+    sort(range, range + n);
+
+    int res = 0, ed = -2e9;
+    for (int i = 0; i < n; i++) {
+        if (range[i].l > ed) {
+            res++;
+            ed = range[i].r;
+        }
+    }
+
+    printf("%d\n", res);
+
+    return 0;
+}
