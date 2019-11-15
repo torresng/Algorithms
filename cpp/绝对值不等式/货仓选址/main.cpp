0724c2c3e90e5886f03d58b25cfc87c7953bb6d1
Torres Ng <torresng2684cte@gmail.com>
2019-11-15 20:13:10 +0800

    绝对值不等式

---
+++ cpp/绝对值不等式/货仓选址/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int n;
+int a[N];
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    sort(a, a + n);
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        res += abs(a[i] - a[n / 2]);
+    }
+
+    printf("%d\n", res);
+
+    return 0;
+}
